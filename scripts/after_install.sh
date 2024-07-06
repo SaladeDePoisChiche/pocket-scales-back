67b0c8ced1b83ad3e230bb5bd39e13d9c24df2f5
Soledad <soledadcot@gmail.com>
2024-07-06 18:54:50 +0200

    fix: added run build in deployment script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,3 +6,6 @@ cd /home/ec2-user/pocket-scales-back >> /home/ec2-user/pocket-scales-back/deploy
 
 echo 'npm install' >> /home/ec2-user/pocket-scales-back/deploy.log 
 npm install >> /home/ec2-user/pocket-scales-back/deploy.log
+
+echo 'npm run build' >> /home/ec2-user/pocket-scales-back/deploy.log 
+npm run build >> /home/ec2-user/pocket-scales-back/deploy.log 
