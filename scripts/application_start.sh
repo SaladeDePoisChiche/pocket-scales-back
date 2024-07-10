642ab265c8aa25ae7e019710a332c830e6cfab8d
Soledad <soledadcot@gmail.com>
2024-07-10 17:42:36 +0200

    fix: added sudo to file

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@ date -u >> /home/ec2-user/pocket-scales-back/deploy.log
 echo 'run application_start.sh: ' >> /home/ec2-user/pocket-scales-back/deploy.log
 # nodejs-app is the same name as stored in pm2 process
 echo 'pm2 restart nodejs-app' >> /home/ec2-user/pocket-scales-back/deploy.log
-pm2 restart ./dist/main.js >> /home/ec2-user/pocket-scales-back/deploy.log
+sudo pm2 restart ./dist/main.js >> /home/ec2-user/pocket-scales-back/deploy.log
