ee8a40d4cb04cea863624d92815607ae931a5bbc
Soledad <soledadcot@gmail.com>
2024-07-10 17:45:40 +0200

    fix: modified server.ts to test + changed logs

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,5 @@ date -u >> /home/ec2-user/pocket-scales-back/deploy.log
 
 echo 'run application_start.sh: ' >> /home/ec2-user/pocket-scales-back/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'pm2 restart nodejs-app' >> /home/ec2-user/pocket-scales-back/deploy.log
+echo 'sudo pm2 restart nodejs-app' >> /home/ec2-user/pocket-scales-back/deploy.log
 sudo pm2 restart ./dist/main.js >> /home/ec2-user/pocket-scales-back/deploy.log
