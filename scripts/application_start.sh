8350ca339039e5ec335b123bbcc4960efb41dec3
Soledad <130313956+SaladeDePoisChiche@users.noreply.github.com>
2024-07-11 18:44:51 +0200

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 date -u >> /home/ec2-user/pocket-scales-back/deploy.log
 
+echo 'current launch location:' >> /home/ec2-user/pocket-scales-back/deploy.log
+pwd >> /home/ec2-user/pocket-scales-back/deploy.log
+
 echo 'run application_start.sh: ' >> /home/ec2-user/pocket-scales-back/deploy.log
 # nodejs-app is the same name as stored in pm2 process
 echo 'sudo pm2 restart nodejs-app' >> /home/ec2-user/pocket-scales-back/deploy.log
