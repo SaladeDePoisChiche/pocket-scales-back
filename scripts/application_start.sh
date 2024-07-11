45ff32ef2efefad448da3ca9f70d0ef80388bcb2
Soledad <soledadcot@gmail.com>
2024-07-11 19:12:23 +0200

    fix: application start script path

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,5 +6,5 @@ pwd >> /home/ec2-user/pocket-scales-back/deploy.log
 
 echo 'run application_start.sh: ' >> /home/ec2-user/pocket-scales-back/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'sudo pm2 restart nodejs-app' >> /home/ec2-user/pocket-scales-back/deploy.log
-sudo pm2 restart ./dist/main.js >> /home/ec2-user/pocket-scales-back/deploy.log
+echo 'pm2 restart nodejs-app' >> /home/ec2-user/pocket-scales-back/deploy.log
+pm2 restart /home/ec2-user/pocket-scales-back/dist/main.js >> /home/ec2-user/pocket-scales-back/deploy.log
